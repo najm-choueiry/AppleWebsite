95a2b092fdda6e67dc537b5abb0c85dccc2be2ee
najm-choueiry <nachoueiry@outlook.com>
2024-10-26 12:06:46 +0300

    setting up the video placeholders

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,5 +1,23 @@
+import { hightlightsSlides } from "../constants";
+
 const VideoCarousel = () => {
-  return <div>VideoCarousel</div>;
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, i) => (
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounder-3xl overflow-hidden bg-black">
+                <video id="video" preload="auto" muted>
+                  <source type="video/mp4" src={list.video} />
+                </video>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default VideoCarousel;
